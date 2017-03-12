a0f91bd46fabf2e2856d9217cd3eb89e0f0646fd
Yenney, Greg <gyenney@wdhl155fa33f5.local>
2017-03-12 10:41:00 -0700

    Initial Commit

---
+++ GregsTipCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GregsTipCalculator
+//
+//  Created by Yenney, Greg on 3/12/17.
+//  Copyright © 2017 Yenney, Greg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
